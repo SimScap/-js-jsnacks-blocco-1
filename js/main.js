d405c66cf327ca91be59ab3a9d9dfc0b08b54b4c
SimoneScapin <simonescap87@gmail.com>
2022-04-17 12:40:43 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * L'utente inserisce due numeri in successione, con due prompt.
+    Il software stampa il maggiore.
+ */
+
+
+
+/* inizializzare le variabili nel prompt(dialogo) e aggiungere un parseint per essere sicuri che sono numeri interi */
+
+let numberOne = parseInt(prompt("Please enter your first number"));
+console.log(numberOne);
+let numberTwo = parseInt(prompt("Please enter your second number"));
+console.log(numberTwo);
